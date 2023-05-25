4f3a38c130fe62e90c26fd1b711854cc1c2e2437
Neto069333 <133918298+Neto069333@users.noreply.github.com>
2023-05-25 14:07:01 -0600

    4V2TRIFIV

---
+++ 4V2TRIFIV/V4TRIFIV/src/v4trifiv/V4TRIFIV.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package v4trifiv;
+
+/**
+ *
+ * @author ernes
+ */
+public class V4TRIFIV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
